547eda7c2af35e2db09caf9a27d34e299b60e533
Micah Elijah <melijah200@gmail.com>
2021-04-01 10:12:10 +0100

    Restructured project into versioned api

--- v1/index.js
+++ v1/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 
 const v1 = express();
@@ -7,5 +8,9 @@ v1.use(express.json());
 const authRoutes = require("./routes/auth");
 
 v1.use("/auth", authRoutes);
+
+v1.get("/", (req, res) => {
+  return res.send("Transaction Runner API v1");
+});
 
 module.exports = v1;
